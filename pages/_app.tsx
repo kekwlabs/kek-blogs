6e1ed27b2f422d44630a26f08df0de7bc92df097
ujjwal <katanadeveloper01@gmail.com>
2021-10-26 02:11:41 +0530

    trim index.tsx, add chakraprovider and dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const config: ThemeConfig = {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  }
+
+  const theme = extendTheme({ config })
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider> 
+  )
 }
 export default MyApp
