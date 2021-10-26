95f6f7010af420fbd6ac8905f18bc74777c50869
ujjwal <katanadeveloper01@gmail.com>
2021-10-27 00:25:37 +0530

    home: add dummy content to test styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from 'next'
 import { Container, Text, Button } from '@chakra-ui/react'
 import styles from '../styles/home.module.css'
+import { Blogs, Blog } from '../types/blog'
 
 const Home: NextPage = () => {
-  
+
   let homepage: any = () => {
     return window.open('https://kekwlabs.github.io')
   }
+
+  let blogs =  Blogs.map((blog: Blog, i) => {
+    return (
+    <Container maxW="container.lg" className={styles.blog}>
+      <Text fontSize="3xl">{blog.title}</Text>
+      <Text fontSize="1xl">{blog.description}</Text>
+    </Container>
+    )
+  })
 
   return (
     <main>
@@ -16,6 +26,9 @@ const Home: NextPage = () => {
           <Text className={styles.desc} fontSize="1.1rem">Blog platform for kekwlabs&apos; members to publish research blogs and writups based on computer science and security.</Text>
           <Button onClick={homepage} className={styles.btn} colorScheme="cyan">Join Us</Button>
         </Container>
+        <div className={styles.blogs}>
+            {blogs}
+        </div>
       </section>
     </main>
   )
---
+++ types/blog.ts
@@ -0,0 +1,40 @@
+export interface Blog {
+    title: string
+    description: string
+    body: string
+}
+
+export const Blogs: Blog[] = [
+    {
+        title: 'This is Blog 1',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure quaerat a commodi dolor officiis expedita quasi, eaque reiciendis debitis saepe dolore molestiae maxime distinctio, voluptas qui tenetur quod excepturi optio',
+        body: 'body'
+    },
+
+    {
+        title: 'This is Blog 2',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure quaerat a commodi dolor officiis expedita quasi, eaque reiciendis debitis saepe dolore molestiae maxime distinctio, voluptas qui tenetur quod excepturi optio',
+        body: 'body'
+    },
+
+
+    {
+        title: 'This is Blog 3',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure quaerat a commodi dolor officiis expedita quasi, eaque reiciendis debitis saepe dolore molestiae maxime distinctio, voluptas qui tenetur quod excepturi optio',
+        body: 'body'
+    },
+
+
+    {
+        title: 'This is Blog 4',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure quaerat a commodi dolor officiis expedita quasi, eaque reiciendis debitis saepe dolore molestiae maxime distinctio, voluptas qui tenetur quod excepturi optio',
+        body: 'body'
+    },
+
+
+    {
+        title: 'This is Blog 5',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure quaerat a commodi dolor officiis expedita quasi, eaque reiciendis debitis saepe dolore molestiae maxime distinctio, voluptas qui tenetur quod excepturi optio',
+        body: 'body'
+    },
+]
