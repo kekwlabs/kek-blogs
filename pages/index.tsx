9feeae228f6c0147f80a8c8b0ef87e6d8b798675
ujjwal <katanadeveloper01@gmail.com>
2021-10-26 06:54:58 +0530

    basic homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next'
+import { Container, Text } from "@chakra-ui/react"
+import styles from "../styles/home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello</h1>
+    <div className={styles.top}>
+      <Container maxW="container.lg">
+        <Text fontSize="5xl"><code>KekwLabs Blog</code></Text>
+        <br />
+        <Text fontSize="1.1rem"><code>Blog platform for kekwlabs' members to publish research and development content like blogs and writups on computer science and security research</code></Text>
+      </Container>
+    </div>
   )
 }
 
