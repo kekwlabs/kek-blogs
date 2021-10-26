06e51c846f5deabbb5015c6b921a8e4867414426
ujjwal <katanadeveloper01@gmail.com>
2021-10-26 09:57:38 +0530

    home: add join us btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next'
-import { Container, Text } from "@chakra-ui/react"
-import styles from "../styles/home.module.css";
+import { Container, Text, Button } from '@chakra-ui/react'
+import styles from '../styles/home.module.css'
 
 const Home: NextPage = () => {
+  
+  let homepage: any = () => {
+    return window.open('https://kekwlabs.github.io')
+  }
+
   return (
     <main>
       <section className={styles.top}>
         <Container maxW="container.lg">
           <Text as="b" className={styles.mono} fontSize="4xl" color="cyan.500">KekwLabs Blog</Text>
           <Text className={`${styles.mono} ${styles.desc}`} fontSize="1.1rem">Blog platform for kekwlabs' members to publish research blogs and writups based on computer science and security.</Text>
+          <Button onClick={homepage} className={styles.btn} colorScheme="cyan">Join Us</Button>
         </Container>
       </section>
     </main>
