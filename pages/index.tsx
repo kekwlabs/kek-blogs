15272370fabf8af104279f3e4c8f26fd7c3fee11
ujjwal <katanadeveloper01@gmail.com>
2021-10-26 08:53:19 +0530

    add some styles to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import styles from "../styles/home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.top}>
-      <Container maxW="container.lg">
-        <Text fontSize="5xl"><code>KekwLabs Blog</code></Text>
-        <br />
-        <Text fontSize="1.1rem"><code>Blog platform for kekwlabs' members to publish research and development content like blogs and writups on computer science and security research</code></Text>
-      </Container>
-    </div>
+    <main>
+      <section className={styles.top}>
+        <Container maxW="container.lg">
+          <Text as="b" className={styles.mono} fontSize="4xl" color="cyan.500">KekwLabs Blog</Text>
+          <Text className={`${styles.mono} ${styles.desc}`} fontSize="1.1rem">Blog platform for kekwlabs' members to publish research blogs and writups based on computer science and security.</Text>
+        </Container>
+      </section>
+    </main>
   )
 }
 
