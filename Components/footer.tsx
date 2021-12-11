e9a557f66cae5867f96888f0293a8d9e0300dd69
Siddharth Arumugam <0xmrnight@gmail.com>
2021-12-11 16:16:45 +0530

    refactor: update footer icons
    Update the github icon (Fixes #13)
    Add the icons to public/assets/icons/ and update refs in footer.tsx

--- Components/footer.tsx
+++ Components/footer.tsx
@@ -1,17 +1,49 @@
 export default function Footer() {
-    return (
-        <footer className="Footer">
-            <p>
-                <a href="https://github.com/orgs/kekwlabs/people">
-                <code>&copy; kekwLabs</code>
-                </a>
-            </p>
-            <ul>
-                <li><a href="https://twitter.com/kekwLabs"><img className="contact-icons" src="https://www.svgrepo.com/show/157815/twitter.svg" /></a></li>
-                <li><a href="https://discord.com/invite/9r88g5FyEU"><img className="contact-icons" src="https://www.svgrepo.com/show/331368/discord-v2.svg" /></a></li>
-                <li><a href="https://www.reddit.com/r/kekwlabs"><img className="contact-icons" src="https://www.svgrepo.com/show/331560/reddit.svg"/></a></li>
-                <li><a href="https://github.com/kekwlabs/"><img className="contact-icons" src="https://www.svgrepo.com/show/355033/github.svg" /></a></li>
-            </ul>
-        </footer>
-    )
+  return (
+    <footer className="Footer">
+      <p>
+        <a href="https://github.com/orgs/kekwlabs/people">
+          <code>&copy; kekwLabs</code>
+        </a>
+      </p>
+      <ul>
+        <li>
+          <a href="https://twitter.com/kekwLabs">
+            <img
+              className="contact-icons"
+              src="/assets/icons/twitter.svg"
+              alt=""
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://discord.com/invite/9r88g5FyEU">
+            <img
+              className="contact-icons"
+              src="/assets/icons/discord.svg"
+              alt=""
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.reddit.com/r/kekwlabs">
+            <img
+              className="contact-icons"
+              src="/assets/icons/reddit.svg"
+              alt=""
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/kekwlabs/">
+            <img
+              className="contact-icons"
+              src="/assets/icons/github.svg"
+              alt=""
+            />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
